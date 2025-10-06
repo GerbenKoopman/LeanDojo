71c38aab18cf78289f2b463d244adc1b3d91f7e4
Gerben Koopman <gerbennkoopman@gmail.com>
2025-10-06 22:55:53 +0200

    feat: implement demo notebook in python file for snellius

---
+++ scripts/demo-lean4.py
@@ -0,0 +1,43 @@
+from lean_dojo import *
+from pathlib import Path
+
+repo = LeanGitRepo(
+    "https://github.com/leanprover-community/mathlib4",
+    "29dcec074de168ac2bf835a77ef68bbe069194c5",
+)
+
+traced_repo = trace(repo)
+
+traced_file = traced_repo.get_traced_file("Mathlib/Algebra/BigOperators/Pi.lean")
+
+traced_theorems = traced_file.get_traced_theorems()
+
+print(len(traced_theorems))
+
+thm = traced_file.get_traced_theorem("pi_eq_sum_univ")
+
+if thm is None:
+    raise ValueError("Theorem not found")
+
+proof_node = thm.get_proof_node()
+proof = proof_node.lean_file[proof_node.start : proof_node.end]
+print(proof)
+
+traced_tactics = thm.get_traced_tactics()
+
+tac = traced_tactics[1]
+
+theorem = Theorem(repo, Path("Mathlib/Algebra/BigOperators/Pi.lean"), "pi_eq_sum_univ")
+
+# For some theorems, it might take a few minutes.
+dojo, state_0 = Dojo(theorem).__enter__()
+
+print(state_0.pp)
+
+state_1 = dojo.run_tac(state_0, "revert x")
+
+print(state_1.pp)
+
+state_2 = dojo.run_tac(state_0, "hello world!")
+
+print(state_2.pp)
